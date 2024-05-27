22d97d068547399cf0e5bd42d68254bee39b9083
“Maggie <mzhang.ark@gmail.com>
2024-05-27 19:03:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const knex = require("knex")(require("./knexfile"));
+// const apiRoutes = require("./routes/apiRoutes");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8080;
+const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:5173";
+const app = express();
+
+// app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json());
+
+// app.use("/api", apiRoutes);
+
+
+app.listen(PORT, () =>{
+    console.log("Server is listening on " +  `port ${PORT}`);
+})

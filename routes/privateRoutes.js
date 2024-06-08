5cdae151b276ae6b016390ad0233b62738555009
“Maggie <mzhang.ark@gmail.com>
2024-06-08 15:36:03 -0400

    added updateUser

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -2,13 +2,22 @@ const router = require("express").Router();
 const authController = require("../controllers/auth-controller");
 const userController = require("../controllers/user-controller");
 
-// router
-//   .route("/users")
-  // .get(userController.getAllUsers)
-  // .get(authController.loginUser);
+router
+  .route("/users")
+  .get(userController.getAllUsers)
+//   .get(authController.loginUser);
 
-router.route("/users/register").post(authController.postUser);
+router
+.route("/users/register")
+.post(authController.postUser);
 
-router.route("/users/:userId").get(userController.getOneUser);
+router
+.route("/users/login")
+.post(authController.loginUser)
+
+router
+.route("/users/:userId")
+.patch(authController.updateUser);
+
 
 module.exports = router;
